aab47ba4e06ec384cfbd93b9d6027bfa90752c2a
SebastianLL <71473385+SebastianLL@users.noreply.github.com>
2020-11-19 03:12:35 -0700

    Add files via upload

---
+++ Proyecto final/inicio.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Inicio - Proyecto final
+	</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="assets/estilos.css">
+</head>
+<body>
+
+	<div class="contenedor-total">
+		
+
+		<div class="header">
+			
+			<div class="header-fijo">
+				
+
+				<div class="fijo-izquierda">
+					
+					<img src="imagenes/netflix-logo.png" height="100">
+
+					<a class="direcciones" href="">Inicio</a>
+					<a class="direcciones" href="">Series</a>
+					<a class="direcciones" href="">Peliculas</a>
+					<a class="direcciones" href="">Más recientes</a>
+					<a class="direcciones" href="">Mi lista</a>
+
+				</div>
+
+				<div class="fijo-derecha">
+					
+
+					<img class="direccionesDer" src="imagenes/lupa.png" height="30">
+					<a class="direcciones3" href="">Niños</a>
+					<img class="direcciones2" src="imagenes/regalo.png" height="30">
+					<img class="direcciones2" src="imagenes/notificacion.png" height="30">
+					<img class="direcciones2" src="imagenes/usuario.png" height="30">
+
+				</div>
+
+			</div>
+
+			<img src="imagenes/casa.jpg" height="700" width="1350">
+
+			
+
+		</div>
+
+	</div>
+
+
+</body>
+</html>
---
+++ Proyecto final/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Login - Proyecto final
+	</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="assets/estilos.css?v=0.0.1">
+</head>
+<body>
+
+	<div class="contenedor">
+
+		<img class="logo" src="imagenes/netflix-logo.png" height="200">
+
+		<img class="fondo" src="imagenes/fondo.jpg" height="657" width="1400">
+
+		<div class="form-login">
+			
+			<h2>
+				Inicia sesión
+			</h2>
+			<input class="input-sesion" type="text" name="email" placeholder="Email o número de teléfono" size="45">
+			<br>
+			<br>
+			<input class="input-sesion" type="password" name="password" placeholder="Contraseña" size="45">
+			<br>
+			<br>
+			<br>
+			<br>
+			<button class="btn-sesion">Inisiar sesión</button>
+			<p class="ayuda-sesion"><a href="">¿Necesitas ayuda?</a></p>
+
+		</div>
+
+
+
+		
+	</div>
+
+</body>
+</html>
